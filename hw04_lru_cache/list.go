e1e1f96cd739f6247d474253e8c93c392c5af7b8
Roman Mikhailov <roman.mikhailov@MacBook-Pro-Roman.local>
2024-01-12 22:25:49 +0300

    list implementation

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,10 +17,105 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	elements map[*ListItem]struct{}
+	front    *ListItem
+	back     *ListItem
+}
+
+func (l *list) Len() int {
+	return len(l.elements)
+}
+
+func (l *list) Front() *ListItem {
+	return l.front
+}
+
+func (l *list) Back() *ListItem {
+	return l.back
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	newFront := ListItem{
+		Next:  l.front,
+		Prev:  nil,
+		Value: v,
+	}
+
+	if len(l.elements) == 0 {
+		l.back = &newFront
+		l.front = &newFront
+	} else {
+		l.front.Prev = &newFront
+		l.front = &newFront
+	}
+
+	l.elements[&newFront] = struct{}{}
+	return l.front
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	newBack := ListItem{
+		Next:  nil,
+		Prev:  l.back,
+		Value: v,
+	}
+
+	if len(l.elements) == 0 {
+		l.back = &newBack
+		l.front = &newBack
+	} else {
+		l.back.Next = &newBack
+		l.back = &newBack
+	}
+
+	l.elements[&newBack] = struct{}{}
+	return l.back
+}
+
+func (l *list) Remove(i *ListItem) {
+	if len(l.elements) == 0 {
+		return
+	}
+
+	if _, exists := l.elements[i]; exists {
+		if i.Prev == nil && i.Next == nil {
+			l.front = nil
+			l.back = nil
+		} else if i.Prev == nil {
+			l.back = i.Next
+		} else if i.Next == nil {
+			l.front = i.Prev
+		} else {
+			i.Next.Prev = i.Prev
+			i.Prev.Next = i.Next
+		}
+		delete(l.elements, i)
+	}
+
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	if _, exists := l.elements[i]; exists {
+		if i == l.front {
+			return
+		} else if i == l.back {
+			l.back = i.Prev
+			i.Prev.Next = nil
+			i.Next = l.front
+			l.front.Prev = i
+
+		} else {
+			i.Prev = nil
+			i.Next = l.front
+			i.Next.Prev = i.Prev
+			l.front.Prev = i
+		}
+		l.front = i
+	}
 }
 
 func NewList() List {
-	return new(list)
+	return &list{
+		elements: make(map[*ListItem]struct{}),
+	}
 }
