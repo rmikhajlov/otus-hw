ff89aba25c9b3ea12f00a7151df1fc7fb631c401
Roman Mikhailov <roman.mikhailov@MacBook-Pro-Roman.local>
2024-02-07 22:54:32 +0300

    copy is working, test.sh PASS

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
 )
 
 var (
@@ -10,6 +13,39 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
-	return nil
+
+	fileFrom, err := os.OpenFile(fromPath, os.O_RDONLY, 0)
+	if err != nil {
+		return fmt.Errorf("error while opening file: %v", err)
+	}
+	defer fileFrom.Close()
+
+	fileFromStat, err := fileFrom.Stat()
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+
+	if offset > fileFromStat.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	_, err = fileFrom.Seek(offset, 0)
+	if err != nil {
+		return fmt.Errorf("error while seeking file: %v", err)
+	}
+
+	fileTo, err := os.Create(toPath)
+	if err != nil {
+		return fmt.Errorf("error while creating file: %v", err)
+	}
+	defer fileTo.Close()
+
+	if limit == 0 || limit > fileFromStat.Size() {
+		_, err = io.Copy(fileTo, fileFrom)
+	} else {
+		fmt.Println("COPIED PART")
+		_, err = io.CopyN(fileTo, fileFrom, limit)
+	}
+
+	return err
 }
