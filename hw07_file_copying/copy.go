6897b0af05dfb920840d7e3de9efc355fc98b639
Roman Mikhailov <roman.mikhailov@MacBook-Pro-Roman.local>
2024-02-13 00:02:53 +0300

    HW7 done

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -3,9 +3,10 @@ package main
 import (
 	"errors"
 	"fmt"
-	"github.com/cheggaaa/pb/v3"
 	"io"
 	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -14,10 +15,13 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
+	if offset < 0 || limit < 0 {
+		return fmt.Errorf("offset and limit should be >= 0")
+	}
 
 	fileFrom, err := os.OpenFile(fromPath, os.O_RDONLY, 0)
 	if err != nil {
-		return fmt.Errorf("error while opening file: %v", err)
+		return ErrUnsupportedFile
 	}
 	defer fileFrom.Close()
 
@@ -32,36 +36,38 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 
 	_, err = fileFrom.Seek(offset, 0)
 	if err != nil {
-		return fmt.Errorf("error while seeking file: %v", err)
+		return fmt.Errorf("error while seeking file: %w", err)
 	}
 
 	fileTo, err := os.Create(toPath)
 	if err != nil {
-		return fmt.Errorf("error while creating file: %v", err)
+		return fmt.Errorf("error while creating file: %w", err)
 	}
 	defer fileTo.Close()
 
 	var pbLimit int64
 
-	if limit == 0 {
+	if limit+offset > fileFromStat.Size() {
 		pbLimit = fileFromStat.Size() - offset
 	} else {
-		pbLimit = limit - offset
+		pbLimit = limit
 	}
 
 	bar := pb.Full.Start64(pbLimit)
 
 	barReader := bar.NewProxyReader(fileFrom)
 
-	if limit == 0 || limit > fileFromStat.Size() {
+	if pbLimit == 0 || pbLimit > fileFromStat.Size() {
 		_, err = io.Copy(fileTo, barReader)
 		if err != nil {
-			return fmt.Errorf("error while copying file: %v", err)
+			os.Remove(toPath)
+			return fmt.Errorf("error while copying file: %w", err)
 		}
 	} else {
-		_, err = io.CopyN(fileTo, barReader, limit)
+		_, err = io.CopyN(fileTo, barReader, pbLimit)
 		if err != nil {
-			return fmt.Errorf("error while copying file: %v", err)
+			os.Remove(toPath)
+			return fmt.Errorf("error while copying file: %w", err)
 		}
 	}
 
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,45 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("offset exceeds file size", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "testdata/test_result.txt", 7000, 0)
+		require.True(t, errors.Is(err, ErrOffsetExceedsFileSize), "actual err - %v", err)
+	})
+
+	t.Run("error while opening file", func(t *testing.T) {
+		err := Copy("testdata/not_exist.txt", "testdata/test_result.txt", 0, 0)
+		require.Error(t, err, "expected error, got nil")
+	})
+
+	t.Run("error while creating file", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "testdata/not_exist/test_result.txt", 0, 0)
+		require.Error(t, err, "expected error, got nil")
+	})
+
+	t.Run("negative offset", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "testdata/test_result.txt", -10, 0)
+		require.Error(t, err, "expected error, got nil")
+	})
+
+	t.Run("negative limit", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "testdata/test_result.txt", 0, -10)
+		require.Error(t, err, "expected error, got nil")
+	})
+
+	t.Run("error while copying the file", func(t *testing.T) {
+		err := Copy("testdata", "testdata/test_result.txt", 0, 0)
+		require.Error(t, err, "expected error, got nil")
+	})
+
+	t.Run("unsupported file", func(t *testing.T) {
+		err := Copy("~/dev/urandom", "testdata/test_result.txt", 0, 0)
+		require.True(t, errors.Is(err, ErrUnsupportedFile), "actual err - %v", err)
+	})
 }
